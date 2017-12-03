98e94b53162124e8c20befd98e6deb891a2957a1
guilima <gui.lima30@gmail.com>
2017-12-03 16:09:12 -0200

    Set youtube apikey

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const ytAPI_KEY = 'AIzaSyAo7oqB94eRfyakj6L7isrzHHAXNYhyMCU';
 const App = () => <div>Hello World!</div>;
 
 ReactDOM.render(<App />, document.querySelector('.container'));
