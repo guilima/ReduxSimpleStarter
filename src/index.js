06386e65ba58a6b5a08a7462091626d717993fb2
Guilherme Carvalho Lima <guilherme.clima@b2wdigital.com>
2017-12-28 16:26:01 -0200

    refactor(redux): Active redux devtool on browser

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const createStoreWithMiddleware = composeEnhancers(applyMiddleware())(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
